bb38e0fb60a68f30836c92ff5e0917c18a63d5e6
rolo <rolando.rosales@pm.me>
2024-03-07 16:37:12 -0600

    Remove unused function and initialize VL53L4CX sensor

--- Software/VL53L4CX_i2c_control/src/main.cpp
+++ Software/VL53L4CX_i2c_control/src/main.cpp
@@ -1,19 +1,10 @@
 #include <Arduino.h>
 #include <VL53L0X_i2c.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  initVL53L4CX();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
