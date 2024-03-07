1b8d4cc419dcdc3ce3878642f6654ec47c58ee97
rolo <rolando.rosales@pm.me>
2024-03-07 16:36:36 -0600

    added vl53- i2c project

---
+++ Software/VL53L4CX_i2c_control/lib/VL53L4CX_i2c/src/VL53L4CX_i2c.cpp
@@ -0,0 +1,7 @@
+#ifndef VL53L4CX_I2C_H_
+#define VL53L4CX_I2C_H_
+
+// Function declaration
+void initVL53();
+
+#endif
---
+++ Software/VL53L4CX_i2c_control/lib/VL53L4CX_i2c/src/VL53L4CX_i2c.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include "VL53L4CX_i2c.h"
+
+void initVL53L4CX() {
+    
+}
