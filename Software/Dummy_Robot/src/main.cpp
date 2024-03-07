01389142c10a2220a46f2337ed8d611bbaa9bf91
rolo <rolando.rosales@pm.me>
2024-03-06 23:38:51 -0600

    Added initial code for dummy robot. Currently does not work.

--- Software/Dummy_Robot/src/main.cpp
+++ Software/Dummy_Robot/src/main.cpp
@@ -1,18 +1,69 @@
-#include <Arduino.h>
+/*
+  Bit Bangers Dummy Robot Project
+  github.com/Bit-Bangers-UTA/Senior-Design
 
-// put function declarations here:
-int myFunction(int, int);
+  ESP32-C3 to SmartCar Pinout:
+  STBY: 0 -> 3
+  PWMA: 1 -> 5
+  PWMB: 2 -> 6
+  AINA: 3 -> 7
+  BIN1: 10 -> 8
+*/
+
+#include <Arduino.h>
+#include <XboxSeriesXControllerESP32_asukiaaa.hpp>
+#include <SparkFun_TB6612.h>
+
+#define AIN1 3
+#define BIN1 10
+#define PWMA 1
+#define PWMB 2
+#define STBY 0
+
+XboxSeriesXControllerESP32_asukiaaa::Core xboxController;
+
+const int offsetA = 1;
+const int offsetB = 1;
+
+Motor motor1 = Motor(AIN1, 0, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, 0, PWMB, offsetB, STBY);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  xboxController.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  xboxController.onLoop();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (xboxController.isConnected()) {
+    // at the moment, only goes forward and backward
+    /*
+    // uses right stick to steer
+    Serial.print("Steering: ");
+    float joyRHoriNormalized = (float)xboxController.xboxNotif.joyRHori / XboxControllerNotificationParser::maxJoy;
+    float joyRHoriMapped = joyRHoriNormalized * 2 - 1;
+    Serial.println(joyRHoriMapped, 2);
+    */
+
+    // uses right trigger to accelerate
+    Serial.print("Accelerating at: ");
+    Serial.println((uint16_t)xboxController.xboxNotif.trigRT / 4);
+
+    // uses left trigger to reverse
+    Serial.print("Reversing at: ");
+    Serial.println((uint16_t)xboxController.xboxNotif.trigLT / 4);
+
+    Serial.println();
+  }
+  else {
+    Serial.println("Not connected");
+    
+    if (xboxController.getCountFailedConnection() > 2) {
+      ESP.restart();
+    }
+  }
+
+  motor1.drive((uint16_t)xboxController.xboxNotif.trigRT / 4 + (-1 * (uint16_t)xboxController.xboxNotif.trigRT / 4), 1);
+  motor2.drive((uint16_t)xboxController.xboxNotif.trigRT / 4 + (-1 * (uint16_t)xboxController.xboxNotif.trigRT / 4), 1);
 }
