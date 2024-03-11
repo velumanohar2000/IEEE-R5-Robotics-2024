7bb01ac1e7cb915df2af2834e0d5323d40008182
rolo <rolando.rosales@pm.me>
2024-03-10 19:43:43 -0500

    Added both kill switch receiver and transmitter projects

---
+++ Software/Testing/IR_Kill_SW_RX/src/main.cpp
@@ -0,0 +1,57 @@
+/*
+ * Bit Bangers IR "Kill Switch" Receiver Library
+ * github.com/Bit-Bangers-UTA/Senior-Design
+ *
+ * Authors: 
+ * Rolando Rosales
+ *
+ * Hardware setup:
+ * IR receiver data pin connected to GPIO 3
+ * 
+ * Requires crankyoldgit/IRremoteESP8266@^2.8.6 library
+ */
+
+// Libraries ------------------------------------------------------------------
+
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+
+// Defines --------------------------------------------------------------------
+
+#define IR_RX_PIN 3
+
+// Variables ------------------------------------------------------------------
+
+decode_results results;
+const uint32_t kill_code = 0x1A54FB9;
+bool LED_STATE = false;
+
+// Structures & Classes -------------------------------------------------------
+
+IRrecv irrecv(IR_RX_PIN);
+
+// Functions ------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(115200);
+  
+  irrecv.enableIRIn(); // Start the receiver
+
+  pinMode(LED_BUILTIN, OUTPUT); // Set the LED pin as an output
+  digitalWrite(LED_BUILTIN, LOW); // Turn off in case it is on
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.print("Received IR code: ");
+    Serial.println(results.value, HEX); // Print received code in hex
+
+    if (results.value == kill_code) { // Toggles LED if code is unalive code
+      LED_STATE = !LED_STATE;
+      digitalWrite(LED_BUILTIN, LED_STATE ? HIGH : LOW);
+    }
+
+    irrecv.resume(); // Receive the next value
+  }
+}
---
+++ Software/Testing/IR_Kill_SW_TX/src/main.cpp
@@ -0,0 +1,79 @@
+/*
+ * Bit Bangers IR "Kill Switch" Transmitter Library
+ * github.com/Bit-Bangers-UTA/Senior-Design
+ *
+ * Authors: 
+ * Rolando Rosales
+ *
+ * Hardware setup:
+ * IR LED between Vcc and NPN transistor C
+ * 470 Ohm resistor between GPIO 2 and NPN transistor B
+ * GND connected to NPN transistor E
+ * Push button between Vcc and GPIO 1
+ * 
+ * Requires crankyoldgit/IRremoteESP8266@^2.8.6 library
+ */
+
+// Libraries ------------------------------------------------------------------
+
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+// Defines --------------------------------------------------------------------
+
+#define PUSH_BUTTON 1 // Push button connected to pin 3
+#define IR_TX_PIN 2 // IR LED connected to pin 2
+
+// Variables ------------------------------------------------------------------
+
+// IR control
+const uint32_t kill_code = 0x1A54FB9; // Code to unalive the robot
+
+// Debounce
+bool lastButtonState = LOW; // Previous state of the button
+bool reading = LOW; // Current reading from the button
+uint8_t buttonState = 0; // Current state of the button
+uint8_t debounceDelay = 10; // Debounce delay in milliseconds
+uint32_t lastDebounceTime = 0; // Last time the button's state changed
+
+// Structures & Classes -------------------------------------------------------
+
+IRsend irsend(IR_TX_PIN); // IRsend object to send IR codes
+
+// Functions ------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PUSH_BUTTON, INPUT_PULLDOWN); // Needed as connected to Vcc
+
+  irsend.begin(); // Initialize the IRsend object
+
+  pinMode(LED_BUILTIN, OUTPUT); // Set the LED pin as an output
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the LED
+}
+
+void loop() {
+  bool reading = digitalRead(PUSH_BUTTON);
+  
+  // Check if the button state has changed
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis(); // Reset the debouncing timer
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // If state has not changed for longer than delay, consider it stable
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        irsend.sendNEC(kill_code, 32); // Send the unalive code
+
+        Serial.print("Sent IR code: ");
+        Serial.println(kill_code, HEX); // Print sent code in hex
+      }
+    }
+  }
+
+  lastButtonState = reading; // Save reading
+}
