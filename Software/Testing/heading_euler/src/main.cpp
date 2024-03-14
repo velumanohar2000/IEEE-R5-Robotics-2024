a883c72ea64be65a1635d91e6496539181a7146f
Velu Manohar <velumanohar2000@gmail.com>
2024-03-13 23:04:05 -0500

    Acceleration and heading turned into a library

--- Software/Testing/heading_euler/src/main.cpp
+++ Software/Testing/heading_euler/src/main.cpp
@@ -1,80 +1,43 @@
 #include <Wire.h>
 #include <Adafruit_BNO08x.h>
 #include "math.h"
+#include "BNO085_heading_acceleration.h"
 
 #define BNO08X_RESET -1
 
 Adafruit_BNO08x bno08x;
 sh2_SensorValue_t sensorValue;
-float heading;
 
-void setReports(sh2_SensorId_t reportType, uint32_t interval)
+void setup()
 {
-  Serial.println("Setting desired reports");
-  if (!bno08x.enableReport(reportType, interval)) // Top frequency is about 250Hz but this report is more accurate
-  {
-    Serial.println("Could not enable stabilized remote vector");
-  }
+  setupBNO085(&bno08x);
 }
-void reports()
+
+
+void loop()
 {
-  setReports(SH2_LINEAR_ACCELERATION, 2000);
-  setReports(SH2_ARVR_STABILIZED_RV, 1000);
-}
+  checkReset(&bno08x);
 
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Adafruit BNO08x test!");
-  Wire.begin(9, 8);
-  if (!bno08x.begin_I2C())
-  {
-    Serial.println("Failed to find BNO08x chip");
-    while (1)
-    {
-      delay(10);
-    }
-  }
-  Serial.println("BNO08x Found!");
-  reports();
-  Serial.println("Reading events");
-  delay(1000);
-}
-
-float calculateHeading(float i, float j, float k, float real)
-{
-  float yaw = atan2(2.0f*(i*j+real*k),real*real+i*i-j*j-k*k);
-  yaw *= 180.0f / M_PI;
-
-  // Normalize heading angle
-  if (yaw < 0) {
-    yaw += 360.0f;
-  }
-  return yaw;
-}
-
-void loop() {
-  if (bno08x.wasReset())
-  {
-    Serial.print("sensor was reset ");
-    reports();
-  }
   if (bno08x.getSensorEvent(&sensorValue))
   {
     switch (sensorValue.sensorId)
     {
     case SH2_LINEAR_ACCELERATION:
     {
-      
+      //TODO adjust reports function
+      Serial.print("Acceleration (x): ");
+      float xAccel = sensorValue.un.linearAcceleration.x;
+      Serial.println(xAccel);
+      break;
     }
     case SH2_ARVR_STABILIZED_RV:
     {
-      heading = calculateHeading(sensorValue.un.arvrStabilizedRV.i, sensorValue.un.arvrStabilizedRV.j, sensorValue.un.arvrStabilizedRV.k, sensorValue.un.arvrStabilizedRV.real);
+      float heading = calculateHeading(sensorValue.un.arvrStabilizedRV.i, sensorValue.un.arvrStabilizedRV.j, sensorValue.un.arvrStabilizedRV.k, sensorValue.un.arvrStabilizedRV.real);
+      Serial.print("Heading: ");
+      Serial.println(heading);
+      break;
     }
     }
-  // Print heading
-  Serial.print("Heading: ");
-  Serial.println(heading);
   }
-  delay(100); 
+  delay(100);
 }
