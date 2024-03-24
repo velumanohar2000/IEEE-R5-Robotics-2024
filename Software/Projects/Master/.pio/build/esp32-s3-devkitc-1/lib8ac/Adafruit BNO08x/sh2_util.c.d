cc296dc6a2491e611dc4461c7c8a055ac03d5e0c
Rolando Rosales <93052733+rolo-g@users.noreply.github.com>
2024-03-24 18:00:12 -0500

    Delete Software/Projects/Master directory

--- Software/Projects/Master/.pio/build/esp32-s3-devkitc-1/lib8ac/Adafruit BNO08x/sh2_util.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/lib8ac/Adafruit\ BNO08x/sh2_util.c.o: \
- .pio/libdeps/esp32-s3-devkitc-1/Adafruit\ BNO08x/src/sh2_util.c \
- .pio/libdeps/esp32-s3-devkitc-1/Adafruit\ BNO08x/src/sh2_util.h
