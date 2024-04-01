0ca9ce259e457c8aa64876ffc70f3091390e5a78
rolo <rolando.rosales@pm.me>
2024-03-31 23:17:11 -0500

    Minor changes to VL53L1X test

--- Software/Testing/single_VL53L1X_test/src/main.cpp
+++ Software/Testing/single_VL53L1X_test/src/main.cpp
@@ -4,15 +4,13 @@
   SparkFun Electronics
   Date: April 4th, 2018
   License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
   SparkFun labored with love to create this code. Feel like supporting open source hardware?
   Buy a board from SparkFun! https://www.sparkfun.com/products/14667
-
   This example prints the distance to an object.
-
   Are you getting weird readings? Be sure the vacuum tape has been removed from the sensor.
 */
 
+#include <Arduino.h>
 #include <Wire.h>
 #include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
 
