88f7adcd3326260a05b06c59a95a74ae3210b646
rolo <rolando.rosales@pm.me>
2024-03-31 23:16:42 -0500

    Update IR receiver kill code

--- Software/Testing/IR_Kill_SW_RX/src/main.cpp
+++ Software/Testing/IR_Kill_SW_RX/src/main.cpp
@@ -9,7 +9,7 @@
  * IR receiver data pin connected to GPIO 3
  * 
  * Requires crankyoldgit/IRremoteESP8266@^2.8.6 library
- */
+*/
 
 // Libraries ------------------------------------------------------------------
 
@@ -19,17 +19,17 @@
 
 // Defines --------------------------------------------------------------------
 
-#define IR_TX_PIN 2
+#define IR_RX_PIN 3
 
 // Variables & Constants ------------------------------------------------------
 
 decode_results results;
-const uint32_t kill_code = 0xAF83F03B;
+const uint8_t unalive_code = 0xF;
 bool LED_STATE = false;
 
 // Structures & Classes -------------------------------------------------------
 
-IRrecv irrecv(IR_TX_PIN);
+IRrecv irrecv(IR_RX_PIN);
 
 // Functions ------------------------------------------------------------------
 
@@ -37,9 +37,6 @@ void setup() {
   Serial.begin(115200);
   
   irrecv.enableIRIn(); // Start the receiver
-
-  pinMode(LED_BUILTIN, OUTPUT); // Set the LED pin as an output
-  digitalWrite(LED_BUILTIN, LOW); // Turn off in case it is on
 }
 
 void loop() {
@@ -47,7 +44,7 @@ void loop() {
     Serial.print("Received IR code: ");
     Serial.println(results.value, HEX); // Print received code in hex
 
-    if (results.value == kill_code) { // Toggles LED if code is unalive code
+    if (results.value == IR_RX_PIN) { // Toggles LED if code is unalive code
       LED_STATE = !LED_STATE;
       digitalWrite(LED_BUILTIN, LED_STATE ? HIGH : LOW);
     }
