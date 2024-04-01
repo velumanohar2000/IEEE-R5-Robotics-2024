02057756a487bafbbc7e756791c7a82b4d067dd4
rolo <rolando.rosales@pm.me>
2024-04-01 18:10:31 -0500

    Added deep sleep test for the ESP32-S3

--- Software/Testing/deep_sleep_test/src/main.cpp
+++ Software/Testing/deep_sleep_test/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// const uint8_t wakeup_pin = 17
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(){
+  Serial.begin(115200);
+  delay(1000);
+
+  pinMode(GPIO_NUM_17, INPUT);
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_17, 0); //1 = High, 0 = Low
+
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+  //This is not going to be called
 }
