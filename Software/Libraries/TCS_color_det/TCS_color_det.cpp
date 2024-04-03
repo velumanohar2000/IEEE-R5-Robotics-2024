73fe99c918ab79a3db0755b5faeef55066534ea0
rolo <rolando.rosales@pm.me>
2024-04-03 00:14:41 -0500

    Turned color detection into library, added small library test for sensor

---
+++ Software/Libraries/TCS_color_det/TCS_color_det.cpp
@@ -0,0 +1,180 @@
+/*
+ * Bit Bangers TCS34725 Color Detection Library
+ * github.com/Bit-Bangers-UTA/Senior-Design
+ *
+ * Authors:
+ * Rolando Rosales
+ * 
+ * Comments:
+ * Requires adafruit/Adafruit TCS34725@^1.4.4 library
+*/
+
+// Libraries ------------------------------------------------------------------
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "Adafruit_TCS34725.h"
+
+// Defines --------------------------------------------------------------------
+
+// Preprocessor Directives
+// #define TCS_PRINT_DEBUG
+
+// Color states
+#define RED 1
+#define ORANGE 2
+#define YELLOW 3
+#define GREEN 4
+#define GRAY 5
+#define BLACK 6
+#define PURPLE 7
+#define BLUE 8
+
+// Variables & Constants ------------------------------------------------------
+
+// Ambient color values
+uint16_t r_amb = 0;
+uint16_t g_amb = 0;
+uint16_t b_amb = 0;
+
+// Structures & Classes -------------------------------------------------------
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
+
+// Functions ------------------------------------------------------------------
+
+void initTCS(uint16_t r, uint16_t g, uint16_t b) {
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+
+  r_amb = r;
+  g_amb = g;
+  b_amb = b;
+}
+
+uint8_t getColorCode(void) {
+  uint16_t r_raw, g_raw, b_raw, c;
+  uint8_t color = 0;
+
+  tcs.getRawData(&r_raw, &g_raw, &b_raw, &c); // gets raw color values
+
+  #ifdef TCS_PRINT_DEBUG
+    Serial.println("Raw:");
+    Serial.print("R: "); Serial.print(r_raw, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g_raw, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.println(b_raw, DEC);
+  #endif
+
+  // adjusts color values depending on ambient light
+  int16_t r = r_raw - r_amb;
+  int16_t g = g_raw - g_amb;
+  int16_t b = b_raw - b_amb;
+
+  // if any number is negative, it will float to 0
+  if (r < 0) r = 0;
+  if (g < 0) g = 0;
+  if (b < 0) b = 0;
+
+  #ifdef TCS_PRINT_DEBUG
+    Serial.println("Raw - Ambient:");
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.println(b, DEC);
+  #endif
+
+  // will filter out the lowest value from the RGB values
+  uint16_t lowest = min(min(r, g), b);
+
+  r -= lowest;
+  g -= lowest;
+  b -= lowest;
+
+  #ifdef TCS_PRINT_DEBUG
+    Serial.println("Calibrated:");
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.println(b, DEC);
+  #endif
+
+  if (r > 60) // checks warm colors
+  {
+    if (g < 20) // if the green value is too low, its red
+    {
+      color = RED;
+    }
+    else if ((r - g) < 70) // y is bright. if r and g are close its yellow
+    {
+      color = YELLOW;
+    }
+    else
+    {
+      color = ORANGE; // if g is kinda high, but not close to r its orange
+    }
+  }
+  else if (b > 30) // checks blue colors
+  {
+    if (r < 5) // if there's barely any red, its blue
+    {
+      color = BLUE;
+    }
+    else
+    {
+      color = PURPLE; // if there's noticably some red, its purple
+    }
+  }
+  else if (g > 25) // looks for green
+  {
+    color = GREEN;
+  }
+  else if (r < 13 && g < 13 && b < 13)
+  {
+    color = BLACK; // if too dark, its black
+  }
+  else
+  {
+    color = GRAY; // anything else is just gray
+  }
+
+  #ifdef TCS_PRINT_DEBUG
+    Serial.print("Color is: ");
+    switch (color)
+    {
+      case RED:
+        Serial.println("Red");
+        break;
+      case ORANGE:
+        Serial.println("Orange");
+        break;
+      case YELLOW:
+        Serial.println("Yellow");
+        break;
+      case GREEN:
+        Serial.println("Green");
+        break;
+      case GRAY:  
+        Serial.println("Gray");
+        break;
+      case BLACK: 
+        Serial.println("Black");
+        break;
+      case PURPLE:  
+        Serial.println("Purple");
+        break;
+      case BLUE:  
+        Serial.println("Blue");
+        break;
+      default: 
+        Serial.println("Unknown");
+        break;
+    }
+  Serial.println();
+  #endif
+
+  // neopixelWrite(RGB_BUILTIN, r, g, b); // commented out because of stm32
+  return color;
+}
--- Software/Testing/color_detection/src/main.cpp
+++ Software/Testing/color_detection/src/main.cpp
@@ -4,9 +4,6 @@
  *
  * Authors:
  * Rolando Rosales
- * 
- * Comments:
- * Requires adafruit/Adafruit TCS34725@^1.4.4 library
 */
 
 // Libraries ------------------------------------------------------------------
@@ -14,34 +11,25 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-#include "Adafruit_TCS34725.h"
+#include "TCS_color_det.h"
 
 // Defines --------------------------------------------------------------------
+
+// Preprocessor Directives
+#define PRINT_DEBUG
 
 // I2C Pins
 #define SDA 14
 #define SCL 15
 
-// Color states
-#define RED 1
-#define ORANGE 2
-#define YELLOW 3
-#define GREEN 4
-#define GRAY 5
-#define BLACK 6
-#define PURPLE 7
-#define BLUE 8
+// Ambient color values
+#define R_AMB 14
+#define G_AMB 21
+#define B_AMB 19
 
 // Variables & Constants ------------------------------------------------------
 
-// Ambient color values
-const uint16_t r_amb = 14;
-const uint16_t g_amb = 21;
-const uint16_t b_amb = 19;
-
 // Structures & Classes -------------------------------------------------------
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
 
 // Functions ------------------------------------------------------------------
 
@@ -50,93 +38,11 @@ void setup(void) {
 
   Wire.begin(SDA, SCL);
 
-  if (tcs.begin()) {
-    Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1);
-  }
-
-  pinMode(LED_BUILTIN, OUTPUT);
+  initTCS(R_AMB, G_AMB, B_AMB);
 }
 
 void loop(void) {
-  uint16_t r_raw, g_raw, b_raw, c;
-  uint8_t start_color = 0;
-
-  tcs.getRawData(&r_raw, &g_raw, &b_raw, &c); // gets raw color values
-
-  Serial.println("Raw:");
-  Serial.print("R: "); Serial.print(r_raw, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g_raw, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.println(b_raw, DEC);
-
-  // adjusts color values depending on ambient light
-  int16_t r = r_raw - r_amb;
-  int16_t g = g_raw - g_amb;
-  int16_t b = b_raw - b_amb;
-
-  // if any number is negative, it will float to 0
-  if (r < 0) r = 0;
-  if (g < 0) g = 0;
-  if (b < 0) b = 0;
-
-  Serial.println("Raw - Ambient:");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.println(b, DEC);
-
-  // will filter out the lowest value from the RGB values
-  uint16_t lowest = min(min(r, g), b);
-
-  r -= lowest;
-  g -= lowest;
-  b -= lowest;
-
-  Serial.println("Calibrated:");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.println(b, DEC);
-
-  if (r > 60) // checks warm colors
-  {
-    if (g < 20) // if the green value is too low, its red
-    {
-      start_color = RED;
-    }
-    else if ((r - g) < 70) // y is bright. if r and g are close its yellow
-    {
-      start_color = YELLOW;
-    }
-    else
-    {
-      start_color = ORANGE; // if g is kinda high, but not close to r its orange
-    }
-  }
-  else if (b > 30) // checks blue colors
-  {
-    if (r < 5) // if there's barely any red, its blue
-    {
-      start_color = BLUE;
-    }
-    else
-    {
-      start_color = PURPLE; // if there's noticably some red, its purple
-    }
-  }
-  else if (g > 25) // looks for green
-  {
-    start_color = GREEN;
-  }
-  else if (r < 13 && g < 13 && b < 13)
-  {
-    start_color = BLACK; // if too dark, its black
-  }
-  else
-  {
-    start_color = GRAY; // anything else is just gray
-  }
-  
+  uint8_t start_color = getColorCode();
 
   Serial.print("Color is: ");
   switch (start_color)
@@ -170,6 +76,5 @@ void loop(void) {
       break;
   }
 
-  // neopixelWrite(RGB_BUILTIN, r, g, b); // commented out because of stm32
   Serial.println();
 }
