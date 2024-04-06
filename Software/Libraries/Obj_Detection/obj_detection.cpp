a2e7ec22cf50351246e3119c57db2ad24a8ff92d
NoorPinger <noorpinger2002@gmail.com>
2024-04-06 02:45:16 -0500

    Update Obj_Detection library

---
+++ Software/Libraries/Obj_Detection/obj_detection.cpp
@@ -0,0 +1,108 @@
+#include <obj_detection.h>
+
+SFEVL53L1X distanceSensor;
+OPT3101 opt3101;
+
+#define MAX_SIZE 10
+uint16_t optAverage[MAX_SIZE] = {0};
+uint16_t index = 0;
+uint16_t sumOPT = 0;
+
+double optBuffer(double val)
+{
+  double avg;
+  sumOPT -= optAverage[index];
+  sumOPT += val;
+  optAverage[index] = val;
+  avg = (double)(sumOPT/MAX_SIZE);
+  index = (index + 1) % MAX_SIZE;
+  return avg;
+}
+
+void fillFirstBuffer()
+{
+  for(int i = 0; i < MAX_SIZE; i++)
+  {
+    optBuffer(getWhiskerDistanceCm());
+  }
+}
+
+float getVL53L1XDistanceCm()
+{
+  while(!distanceSensor.checkForDataReady())
+  {
+    delay(1);
+  }
+  float distance = distanceSensor.getDistance()/10;
+  distanceSensor.clearInterrupt();
+  return distance;
+}
+
+
+/*
+This function is for debugging purposes only. It will print out the distance
+*/
+void wallDetection()
+{
+  float distance = getWhiskerDistanceCm();
+  if(distance < 20)
+  {
+    if(getVL53L1XDistanceCm() < 25)
+    {
+      Serial.println("Wall detected");
+    }
+    else
+    {
+      Serial.println("Object detected");
+    }
+  }
+  else
+  {
+    Serial.println("No wall or object detected");
+  }
+}
+
+float X_POS = 10;
+float Y_POS = 10;
+float nextX = 0;
+float nextY = 0;
+
+
+/*
+* @param pseudo: Must always be set, but is not used
+* @description: acts as a blocking function to goToCoordinates in order to avoid obstacles, then returns
+* to the calling goToCoordinates function
+*/
+void wallDetection(bool pseudo)
+{
+  float optDistance; // in cm
+  if(X_POS < 60.48 && Y_POS < 60.48)// if x and y are less than 2ft
+  {
+    optDistance = getWhiskerDistanceCm();
+    if (optDistance < 15)
+    {
+    //   optDistance = getWhiskerDistanceCm() --> redundant?
+    //   if(optDistance < 15)
+    //   {
+    while(optDistance < 15)// turn until we face a direction with no obstacles
+    {
+        turn(CLOCKWISE, 65);
+        delay(500);
+        opeDistance = getWhiskerDistanceCm();// update distance
+    }
+    //   }
+      stopMotors();
+      delay(500); // delay to ensure the robot has stopped (robot has wobble when stopping)
+      move(FORWARD, 255);
+      delay(500);
+      stopMotors();
+    //   goToCoordinates(COORDINATES); no garbage collector, stack gets full
+    }
+  }
+  else
+  {
+    distanceSensor.stopRanging();
+  }
+}
+
+
