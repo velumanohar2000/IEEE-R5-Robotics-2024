3eccaa36ce92f5477d440699bb76d620d4b1b5fe
rolo <rolando.rosales@pm.me>
2024-04-05 20:04:43 -0500

    Update IR pin

--- Software/Testing/IR_RX_sleep_wakeup_test/src/main.cpp
+++ Software/Testing/IR_RX_sleep_wakeup_test/src/main.cpp
@@ -40,7 +40,7 @@
 // IR
 const uint32_t lit_code = 0x574309F6; // Amazon button
 const uint32_t unalive_code = 0x5743D32C; // Netflix button
-gpio_num_t ir_pin = GPIO_NUM_18; // pin number of IR receiver(s)
+gpio_num_t ir_pin = GPIO_NUM_11; // pin number of IR receiver(s)
 
 // Structures & Classes -------------------------------------------------------
 
